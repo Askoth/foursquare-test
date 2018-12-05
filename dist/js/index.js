7956804f0cb9af52f80411ccdbd7631e493537ef
Marcelo Oliveira <askoth@gmail.com>
2018-12-05 20:48:09 +0100

    express and requests

--- dist/js/index.js
+++ dist/js/index.js
@@ -10541,7 +10541,7 @@
 
           axios({
               method: 'get',
-              url: getters.tokenUrl
+              url: 'https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=TWUYXKEMJEFTLQB3VP1OYUFAWV3BFL2NRL3HSTM5L2ULROZR&client_secret=QRSFUHAXZKQEILJ1XL0OIDYOICOYBKYSYBZ3WF4IID5Y2BDZ&v=20181204'//getters.tokenUrl
           }).then(function (res, err) {
 
               console.log(res.data.access_token);
--- src/store/login/actions.js
+++ src/store/login/actions.js
@@ -4,7 +4,7 @@ export default {
     getToken({ getters }) {
         axios({
             method: 'get',
-            url: getters.tokenUrl
+            url: 'https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=TWUYXKEMJEFTLQB3VP1OYUFAWV3BFL2NRL3HSTM5L2ULROZR&client_secret=QRSFUHAXZKQEILJ1XL0OIDYOICOYBKYSYBZ3WF4IID5Y2BDZ&v=20181204'//getters.tokenUrl
         }).then((res, err) => {
 
             if (err) {
